bc4ebc15401afbb240627842b5bdbfb84a2d17b9
Udara-Senevirathna <udaraa.jeewamali@gmail.com>
2023-04-06 13:24:21 +0530

    final bug fix

--- app/src/main/java/com/udara/traveltime/LoginScreen.java
+++ app/src/main/java/com/udara/traveltime/LoginScreen.java
@@ -30,7 +30,7 @@ public class LoginScreen extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginScreen.this,PermissionsActivity.class);
+                Intent intent = new Intent(LoginScreen.this, MapScreen.class);
                 startActivity(intent);
 //                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
 //                    Toast.makeText(LoginScreen.this, "Login Successful!", Toast.LENGTH_SHORT).show();
