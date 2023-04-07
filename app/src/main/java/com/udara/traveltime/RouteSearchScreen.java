c01f5397cb088f10ae0441d4ee21826085eb21e6
Udara-Senevirathna <udaraa.jeewamali@gmail.com>
2023-04-07 19:13:31 +0530

    bug fixing dublicated dependencies

--- app/src/main/java/com/udara/traveltime/RouteSearchScreen.java
+++ app/src/main/java/com/udara/traveltime/RouteSearchScreen.java
@@ -1,12 +1,30 @@
 package com.udara.traveltime;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.udara.traveltime.databinding.ActivityMainBinding;
+import com.udara.traveltime.databinding.ActivityRouteSearchScreenBinding;
+import com.udara.traveltime.fragment.HomeFragment;
+import com.udara.traveltime.fragment.MapsFragment;
+import com.udara.traveltime.fragment.SettingFragment;
 
 public class RouteSearchScreen extends AppCompatActivity {
+
+    ActivityRouteSearchScreenBinding binding;
 
     EditText DepartureLocation;
     EditText ArrivalLocation;
@@ -17,6 +35,41 @@ public class RouteSearchScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_route_search_screen);
+        binding = ActivityRouteSearchScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ReplaceFragment(new HomeFragment());
+
+        binding.navBottomBar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.bottom_home:
+                        ReplaceFragment(new HomeFragment());
+                        break;
+
+                    case R.id.bottom_location:
+                        ReplaceFragment(new MapsFragment());
+                        break;
+
+
+                    case R.id.bottom_setting:
+                        ReplaceFragment(new SettingFragment());
+                        break;
+
+                }
+                return true;
+            }
+        });
+    }
+    private void ReplaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.commit();
+
+
+
     }
 }
---
+++ app/src/main/java/com/udara/traveltime/fragment/MapsFragment.java
@@ -0,0 +1,115 @@
+package com.udara.traveltime.fragment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.TokenWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import android.Manifest;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+//import com.udara.traveltime.Manifest;
+import com.udara.traveltime.R;
+import com.udara.traveltime.databinding.FragmentMapsBinding;
+
+public class MapsFragment extends Fragment implements OnMapReadyCallback{
+
+    FragmentMapsBinding binding;
+    SupportMapFragment mapFragment;
+    private GoogleMap mMap;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        binding = FragmentMapsBinding.inflate(inflater, container, false);
+
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        mapInitialize();
+        return binding.getRoot();
+    }
+
+    private void mapInitialize() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setSmallestDisplacement(16);
+        locationRequest.setFastestInterval(3000);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        Dexter.withContext(getContext())
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+
+                        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+                                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(),
+                                android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            return;
+                        }
+                        mMap.setMyLocationEnabled(true);
+                        fusedLocationProviderClient.getLastLocation().addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getContext(), "error" + e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                            }
+                        }).addOnSuccessListener(new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        Toast.makeText(getContext(), "Permission" +  permissionDeniedResponse.getPermissionName() + "" + "Was denied!", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+    }
+}
