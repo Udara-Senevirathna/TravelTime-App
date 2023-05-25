987b7b4a284ab686f70c47566d4c122fd55cbfc7
Gangula Sandaru Dinusantha <119031640+Gangula-Sandaru@users.noreply.github.com>
2023-05-25 22:49:20 +0530

    finished the RWDaratoReadbullr.

--- app/src/main/java/com/udara/traveltime/RWDataToFirebase.java
+++ app/src/main/java/com/udara/traveltime/RWDataToFirebase.java
@@ -1,4 +1,39 @@
 package com.udara.traveltime;
 
-public class RWDataToFirebase {
+import java.io.Serializable;
+
+public class RWDataToFirebas implements Serializable {
+
+    private String getfirstName, getlastName, getNIC;
+
+    public RWDataToFirebas(){}
+
+    public RWDataToFirebas(String firstName, String lastName, String getNIC){
+        this.getfirstName = firstName;
+        this.getlastName = lastName;
+        this.getNIC = getNIC;
+    }
+    // setters
+    public void setFname(String firstName){
+        this.getfirstName = firstName;
+
+    }
+    public void setLname(String lastName){
+        this.getlastName = lastName;
+    }
+    public void setNIC(String nic){
+        this.getNIC = nic;
+    }
+
+    // getters
+
+    public String getFName(){
+        return this.getfirstName;
+    }
+    public String getLname(){
+        return this.getlastName;
+    }
+    public String getNIC(String nic){
+        return  this.getNIC;
+    }
 }
