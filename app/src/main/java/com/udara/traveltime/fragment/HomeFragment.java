ce48d35a5d6c7bd38d6a765807587a4beb17a8b7
Gangula Sandaru Dinusantha <119031640+Gangula-Sandaru@users.noreply.github.com>
2023-05-26 23:23:40 +0530

    implimented the QueryParcelable

--- app/src/main/java/com/udara/traveltime/fragment/HomeFragment.java
+++ app/src/main/java/com/udara/traveltime/fragment/HomeFragment.java
@@ -24,6 +24,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.udara.traveltime.QueryParcelable;
 import com.udara.traveltime.R;
 import com.udara.traveltime.ResultScreen;
 
