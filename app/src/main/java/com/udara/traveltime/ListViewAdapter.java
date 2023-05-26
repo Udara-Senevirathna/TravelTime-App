f21fd0534141d75ce689d66e7d162ea8a1983f24
Gangula Sandaru Dinusantha <119031640+Gangula-Sandaru@users.noreply.github.com>
2023-05-27 01:44:37 +0530

    edit some java files

--- app/src/main/java/com/udara/traveltime/ListViewAdapter.java
+++ app/src/main/java/com/udara/traveltime/ListViewAdapter.java
@@ -31,6 +31,10 @@ public class ListViewAdapter extends RecyclerView.Adapter<MyListHolder> {
         holder.depatureLocation.setText(list_items.get(position).getDepature_location());
         holder.arrivalLocation.setText(list_items.get(position).getArrival_location());
         holder.time.setText(list_items.get(position).getTime());
+        holder.price.setText(list_items.get(position).getPrice());
+        holder.bus_no.setText(list_items.get(position).bus_no);
+        holder.route_no.setText(list_items.get(position).route_no);
+//        holder.route_button_id.setText(list_items.get(position).route_button_id);
 //        holder.image.setImageResource(list_items.get(position).getImage());
     }
 
