d339b02fbefc396d90ac43aef9d55d11172a20b1
Gangula Sandaru Dinusantha <119031640+Gangula-Sandaru@users.noreply.github.com>
2023-05-27 01:27:49 +0530

    modify the list_items

--- app/src/main/java/com/udara/traveltime/list_items.java
+++ app/src/main/java/com/udara/traveltime/list_items.java
@@ -1,14 +1,17 @@
 package com.udara.traveltime;
 
 public class list_items {
-    String depature_location;
-    String arrival_location;
-    String time;
+    String depature_location, arrival_location, time, price, bus_no, route_no, route_button_id;
 
-    public list_items(String Depature_location, String Arrival_location, String Time) {
+
+    public list_items(String Depature_location, String Arrival_location, String Time, String Price, String Bus_no, String Route_no, String Route_button_id) {
         this.depature_location = Depature_location;
         this.arrival_location = Arrival_location;
         this.time = Time;
+        this.price = Price;
+        this.bus_no = Bus_no;
+        this.route_no = Route_no;
+        this.route_button_id = Route_button_id;
     }
 
     public String getDepature_location() {
@@ -33,6 +36,39 @@ public class list_items {
 
     public void setTime(String Time) {
         this.time = Time;
+    }
+
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String Price) {
+        this.price = Price;
+    }
+
+    public String getBus_no() {
+        return time;
+    }
+
+    public void setBus_no(String Bus_no) {
+        this.bus_no = Bus_no;
+    }
+
+    public String getRoute_no() {
+        return route_no;
+    }
+
+    public void setRoute_no(String Route_no) {
+        this.route_no = Route_no;
+    }
+
+    public String getRoute_button_id() {
+        return route_button_id;
+    }
+
+    public void setRoute_button_id(String Route_button_id) {
+        this.route_button_id = Route_button_id;
     }
 
 }
