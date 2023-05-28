ea4ed7f85b879054a9cfb9c0667a588b77a2ca93
Gangula Sandaru Dinusantha <119031640+Gangula-Sandaru@users.noreply.github.com>
2023-05-28 21:12:57 +0530

    initialize the ui componets to the java  code

--- app/src/main/java/com/udara/traveltime/PaymentMethod.java
+++ app/src/main/java/com/udara/traveltime/PaymentMethod.java
@@ -3,12 +3,25 @@ package com.udara.traveltime;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PaymentMethod extends AppCompatActivity {
+
+    EditText cardNumber, holderName, cvv, exdate;
+    Button payBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_method);
+        cardNumber = findViewById(R.id.cardName2);
+        holderName = findViewById(R.id.cardNumber);
+        cvv = findViewById(R.id.Code);
+        exdate = findViewById(R.id.ExpireDate);
+
+
+        payBtn = findViewById(R.id.PayButton);
+
     }
 }
