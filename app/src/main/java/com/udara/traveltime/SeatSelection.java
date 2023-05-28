d7efb7a5ecbe8a5a81144d71f4b15e61462ea8c2
Gangula Sandaru Dinusantha <119031640+Gangula-Sandaru@users.noreply.github.com>
2023-05-28 10:17:25 +0530

    modify the writemakebooking file

--- app/src/main/java/com/udara/traveltime/SeatSelection.java
+++ app/src/main/java/com/udara/traveltime/SeatSelection.java
@@ -1,9 +1,11 @@
 package com.udara.traveltime;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -11,8 +13,15 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.sql.Date;
+import java.util.Calendar;
 
 public class SeatSelection extends AppCompatActivity {
 
@@ -47,6 +56,10 @@ public class SeatSelection extends AppCompatActivity {
 
         tmptext.setText(ButtonId);
         Toast.makeText(this,String.valueOf(buttonClickedId), Toast.LENGTH_SHORT).show();
+
+        // selected seat no
+
+
         // go to the payment page
         ConfirmBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,7 +69,7 @@ public class SeatSelection extends AppCompatActivity {
                     Toast.makeText(SeatSelection.this, "Seat not selected", Toast.LENGTH_SHORT).show();
                 }else{
 
-                    MakeBookingSeat(SeatSlected);
+                    MakeBookingSeat(SeatSlected, buttonClickedId);
                 }
 
 
@@ -66,12 +79,45 @@ public class SeatSelection extends AppCompatActivity {
         });
     }
 
-    private void MakeBookingSeat(String seatSlected) {
-        // initialize the firebase
+    private void MakeBookingSeat(String seatSelected, String routeId) {
+        // Initialize Firebase
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
 
+        // Get current date and time
+        Calendar calendar = Calendar.getInstance();
 
-        WriteMakeBooking makeBooking = new WriteMakeBooking(seatSlected);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+        String formattedDate = dateFormat.format(calendar.getTime());
+        String formattedTime = timeFormat.format(calendar.getTime());
+
+
+        // Create an instance of WriteMakeBooking with the selected seat, user ID, and route ID
+        WriteMakeBooking makeBooking = new WriteMakeBooking(seatSelected, firebaseUser.getUid(), routeId, formattedDate , formattedTime);
+
+        // Get reference to the "BookingSeat" node in the database
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("BookingSeat");
+
+        // Generate a unique key for the booking
+        String bookingKey = databaseReference.push().getKey();
+
+        // Store the booking under the generated key
+        databaseReference.child(bookingKey).setValue(makeBooking)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Booking stored successfully
+                        // You can perform any further actions here
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Failed to store the booking
+                        // You can handle the error here
+                    }
+                });
     }
+
 }
--- app/src/main/java/com/udara/traveltime/WriteMakeBooking.java
+++ app/src/main/java/com/udara/traveltime/WriteMakeBooking.java
@@ -3,10 +3,14 @@ package com.udara.traveltime;
 import java.io.Serializable;
 
 public class WriteMakeBooking implements Serializable {
-    private String selectedSeat;
+    private String selectedSeat, route_id, userId, date, time;
 
-    public WriteMakeBooking(String selectedSeat) {
+    public WriteMakeBooking(String selectedSeat, String FirebaseUserID, String Route_id, String Date, String Time) {
         this.selectedSeat = selectedSeat;
+        this.route_id = Route_id;
+        this.userId = FirebaseUserID;
+        this.date = Date;
+        this.time = Time;
     }
 
     public String getSelectedSeat() {
@@ -15,5 +19,39 @@ public class WriteMakeBooking implements Serializable {
 
     public void setSelectedSeat(String selectedSeat) {
         this.selectedSeat = selectedSeat;
+    }
+
+
+    public String getRoute_id() {
+        return this.route_id;
+    }
+
+    public void setRoute_id(String Route_id) {
+        this.route_id = Route_id;
+    }
+
+    public String getFirebaseUserID() {
+        return this.userId;
+    }
+
+    public void setFirebaseUserID(String FirebaseUserID) {
+        this.userId = FirebaseUserID;
+    }
+
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public void setDate(String Date) {
+        this.date = Date;
+    }
+
+    public String getTime() {
+        return this.time;
+    }
+
+    public void setTime(String Time) {
+        this.time = Time;
     }
 }
